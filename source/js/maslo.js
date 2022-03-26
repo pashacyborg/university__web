ce73904e460ce2dc8491ffa9533a1d6161aade81
pashacyborg <pashacyborg2@gmail.com>
2022-03-26 10:21:33 +0300

    try of pr3

--- source/js/maslo.js
+++ source/js/maslo.js
@@ -1,7 +1,21 @@
-function maslo() {
-    console.log("opa");
-}
-
-let v = function() {
-
+function Validate() {
+    if(document.myForm.email.value == "") {
+        alert("Пожалуйста проверьте свой email");
+        document.myForm.email.focus()
+        return false
+    } else if(document.myForm.password.value = ""){
+        alert("Пожалуйста проверьте ваш пароль");
+        document.myForm.password.focus()
+        return false
+    } else if(document.myForm.password.length < 3) {
+        alert("Пароль должен содержать не менее трех символов!");
+        document.myForm.password.focus()
+        return false
+    } else if(document.myForm.email.indexOf("@")) {
+       alert("Логин должен содержать символ @");
+        document.myForm.email.focus()
+        return false
+    } else {
+        return true
+    }
 }
